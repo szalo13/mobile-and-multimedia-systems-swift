8a09df504de07baf10ac84ae4b48eabfe8213526
szalo13 <kamil_sza@yahoo.pl>
2017-04-19 13:50:02 +0200

    Initial Commit

---
+++ MobileAndMultimediaSystems/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileAndMultimediaSystems
+//
+//  Created by Kamil Szalek on 19/04/2017.
+//  Copyright © 2017 Kamil Szalek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
