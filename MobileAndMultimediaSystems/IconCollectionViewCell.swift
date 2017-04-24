13cbc4f2f9d0a949bc002e8c89beb29f3459acf2
szalo13 <kamil_sza@yahoo.pl>
2017-04-24 17:49:54 +0200

    Adding images, adding IconCollectionViewCell and IconCollectionViewController

---
+++ MobileAndMultimediaSystems/IconCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  IconCollectionViewCell.swift
+//  MobileAndMultimediaSystems
+//
+//  Created by Kamil Szalek on 24/04/2017.
+//  Copyright © 2017 Kamil Szalek. All rights reserved.
+//
+
+import UIKit
+
+class IconCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var iconPriceLabel: UILabel!
+    
+    
+}
