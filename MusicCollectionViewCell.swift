af52c3b07ee42ea978f43f1f8a30c0298fb681cf
szalo13 <kamil_sza@yahoo.pl>
2017-04-25 16:43:11 +0200

    Adding MusicPlayer list collection controller

---
+++ MusicCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MusicCollectionViewCell.swift
+//  MobileAndMultimediaSystems
+//
+//  Created by Kamil Szalek on 25/04/2017.
+//  Copyright © 2017 Kamil Szalek. All rights reserved.
+//
+
+import UIKit
+
+class MusicCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var albumImage: UIImageView!
+    
+}
